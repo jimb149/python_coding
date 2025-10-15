de8ba707541b6e158645d1a0d9bf0b692afc58ea
James Barr <jamesbarr336@gmail.com>
2025-10-15 12:01:24 +0200

    updates midwayclass

---
+++ session9_coding.py
@@ -0,0 +1,30 @@
+print("Hello world")
+vary23_revised = 1
+print(vary23_revised)
+# lambda = 1  certain words like this cannot be written as a commond or var name
+# how to name our variables in python --> comment
+a = 3
+print(type(a))
+#bc no deciminal points the output calls this 'int' instead of 'float'
+b = 1.2
+print(type(b))   
+c = 'a'
+d = 'abc'
+# double and single quotes are identical for defining string text
+print(d)
+
+# in interactive window can write float(a) then str(a) and you now made a a string variable
+
+#int(b) to round, then float(int(bb)) to make it a deciminal again 
+#cannot turn a string variable into a number
+#doing these commands in the interactive window does not save or override the interactive code but let's us experiment
+
+#Fundamental tuypes of variables
+#integers, float, boolean, complex numbers
+
+i = True
+j = False 
+# these are the boolean variables. it is the same as factor variabnels in stata (1 is true, 0 is false)
+
+# we can do algebra in the interactive window and permanently with the code in boolean
+
