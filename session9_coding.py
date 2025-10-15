dbf44b1720d794d75a7fa5b4fdd9a0db14fa364d
James Barr <jamesbarr336@gmail.com>
2025-10-15 12:27:12 +0200

    end of class 9

--- session9_coding.py
+++ session9_coding.py
@@ -28,3 +28,52 @@ j = False
 
 # we can do algebra in the interactive window and permanently with the code in boolean
 
+# floor division, divides then rounds to nearest whole integer 3.0 // 2.0
+# 2**3 is 2 to the power of 3, do not use the ^ command 
+#single division sign always gives float regardless of result
+# command --> not False outputs True because of Boolean variable 
+print(i+j)
+print(i*j)
+
+# Operators for comparison 
+
+x = 2
+y = 2
+y = 3
+z = 3
+
+x > y 
+# now it has outputs was to weahtehr it is true or false
+b == c
+# checks if b is the same as c and provides an answer 
+
+y is z 
+
+# strings
+s = 'Hello Dumbledore'
+
+# print(s) shows the text and len(s) shows the number of characters
+
+print(s.replace('Dumbledore', 'Snape'))
+
+#s[0:5] shows the characters 0 to 5, with 0 being the first text character and it not including the final cut off paramenter
+#s[-6:] prints the final six characters of the text 
+
+# s[::2] print every second character
+
+s2 = 'Hello Albus Severus'
+s2.split(' ')
+# this split it into three parts with the space as the separator but anything could be a separator
+
+print('Hello \n World')
+
+# \n means start a new line
+#print('Hello \\n World') --> this ignores the next command line 
+
+# 'Monty' + 'Python'
+# ' '.join(['Monty','Pyton']) --> joins these two together 
+
+print("The outcome of interest is", a)
+# print("The outcome of interest is %.2f", a) --> code is currently wrong but the professor will update us on it 
+
+
